a4052dc862eb3d9c7f1b9db1dfffb4c63448a2fa
Tathya <94824143+tathyagarg@users.noreply.github.com>
2023-09-10 10:43:17 +0530

    Update large-3.py
    Optimized sub-optimal code

--- mids/large-3.py
+++ mids/large-3.py
@@ -1,17 +1,10 @@
 # 3. Write a program to read and find the largest of 3 numbers.
 
 def largest(num1 , num2 , num3):
-    if ( num1 >= num2 ) and ( num1 >= num3 ):
-        largest = num1
-    elif ( num2 >= num1 ) and ( num2 >= num3 ):
-        largest = num2
-    elif ( num3 >= num1 ) and ( num3 >= num2 ):
-        largest = num3
-
-    return largest
+    return max(num1, num2, num3) # Since the max func is written in C it's faster than using if statements
 
 num1 = float(input("Enter first number: "))
-num2 = float(input("enter second number: "))
-num3 = float(input("enter third number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
 
 print(f"largest number is {largest(num1,num2,num3)}")
